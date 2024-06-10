d44fdc49ef7a20ef295a93d8fda2718307045fb4
AR2905 <125748114+AR2905@users.noreply.github.com>
2024-06-10 17:39:22 +0530

    Link function, styles, optimization Done

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ let navToggle = document.querySelector(".nav__toggle");
             navToggle.addEventListener("click", function () {
               if (navWrapper.classList.contains("active")) {
                 this.setAttribute("aria-expanded", "false");
+                
                 this.setAttribute("aria-label", "menu");
                 navWrapper.classList.remove("active");
+               
               } else {
                 navWrapper.classList.add("active");
                 this.setAttribute("aria-label", "close menu");
                 this.setAttribute("aria-expanded", "true");
+               
+
               }
             });
